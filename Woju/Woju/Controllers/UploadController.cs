84cef9a0666e7fe6393ab94e1ef5cc3f3ec3aaee
Olamide <olamide.koleosho.1@city.ac.uk>
2016-06-20 13:10:28 +0100

    pre-meeting 20/06/2016

--- Woju/Woju/Controllers/UploadController.cs
+++ Woju/Woju/Controllers/UploadController.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
 using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
+using System.Web.Configuration;
 using System.Web.Mvc;
 
 namespace Woju.Controllers
@@ -27,9 +30,10 @@ namespace Woju.Controllers
             file.SaveAs(filePath);
 
             //save schema to submission directory - currently copies predefined, future add code to generate schema
-            System.IO.File.Copy(fileDirectory + "/schema.ini", submissionDirectory + "/schema.ini", true);
+            System.IO.File.Copy(fileDirectory + "schema.ini", submissionDirectory + "/schema.ini", true);
 
             //import to DB using schema 
+            string localPath = submissionName + "/" + file.FileName;
 
             ViewBag.Import = ImportCsv(filePath, submissionName);
 
@@ -40,26 +44,51 @@ namespace Woju.Controllers
         {
             try
             {
-                
+
                 string query = @"SELECT * INTO {0} 
-                                FROM [Text;HDR=YES;Database={1}]";
+                                FROM OPENQUERY(UploadedCSV, 'select * from {1}')";
 
                 query = String.Format(query, submissionName, filePath);
 
-                using (SqlConnection con = new SqlConnection())
+                Configuration rootWebConfig = WebConfigurationManager.OpenWebConfiguration("/MyWebSiteRoot");
+                System.Configuration.ConnectionStringSettings connString;
+                if (rootWebConfig.ConnectionStrings.ConnectionStrings.Count > 0)
                 {
-                    con.ConnectionString =
-                        @"provider=System.Data.SqlClient; Data Source=.\SQLEXPRESS; AttachDbFilename=|DataDirectory|\Woju.mdf;Integrated Security=True;User Instance=True";
-                    con.Open();
-                    using (SqlCommand cmd = new SqlCommand())
+                    connString = rootWebConfig.ConnectionStrings.ConnectionStrings["WojuDB"];
+                    if (connString != null)
                     {
-                        cmd.Connection = con;
+                        SqlConnection conn = new SqlConnection(connString.ConnectionString);
+                        SqlCommand cmd = new SqlCommand();
+                        
                         cmd.CommandText = query;
+                        cmd.Connection = conn;
+                        cmd.CommandType = CommandType.Text;
+                        conn.Open();
                         cmd.ExecuteNonQuery();
+                        conn.Close();
                     }
-                    con.Close();
+
+
                 }
+                else { return "No connection string"; }
+
+
+
+                //using (SqlConnection con = new SqlConnection())
+                //{
+                //    con.ConnectionString = connString.ConnectionString;
+                //    con.Open();
+                //    using (SqlCommand cmd = new SqlCommand())
+                //    {
+                //        cmd.Connection = con;
+                //        cmd.CommandText = query;
+                //        cmd.ExecuteNonQuery();
+                //    }
+                //    con.Close();
+                //}
                 return "success";
+
+                //return "conn string count > 0";
             }
             catch (Exception ex)
             {
